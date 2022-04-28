eb1bdc571c1ccd78e340ff91d89ec29a329d4abc
Kevin-Med <kevinfurioso_010@hotmail.com>
2022-04-28 15:26:42 +0200

    all_changes

--- server.py
+++ server.py
@@ -3,12 +3,9 @@ import util
 
 app = Flask(__name__)
 
-'''
-
 @app.route('/')
 def home():
     return render_template('app.html')
-'''
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -17,7 +14,7 @@ def get_location_names():
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
-    return response, render_template('app.html')
+    return response
 
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
